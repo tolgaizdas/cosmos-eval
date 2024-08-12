b1eae482fd202296eeb9b62476908108907c8c9f
Tolga İzdaş <tolgaizdas@gmail.com>
2024-08-12 16:48:30 +0300

    completed todo

--- utils.py
+++ utils.py
@@ -20,17 +20,18 @@ def get_log_probs(model, encoded_text):
 
 
 def get_results(model, tokenizer, prompt, choices, device):
+    prompt_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
     results, results_norm = [], []
     for choice in choices:
         unnormalized, normalized = 0.0, 0.0
         byte_length = 0
-        prompt_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)  # TODO: Creating the same prompt_ids for each choice is not efficient
+        current_prompt_ids = prompt_ids.clone()
         choice_ids = tokenizer.encode(choice, add_special_tokens=False)
 
         for c_id in choice_ids:
-            log_probs = get_log_probs(model, prompt_ids)
+            log_probs = get_log_probs(model, current_prompt_ids)
             unnormalized += log_probs[c_id].item()  # Unnormalized (https://blog.eleuther.ai/multiple-choice-normalization/)
-            prompt_ids = torch.cat([prompt_ids, torch.tensor([c_id], device=device).unsqueeze(0)], dim=1)
+            current_prompt_ids = torch.cat([current_prompt_ids, torch.tensor([c_id], device=device).unsqueeze(0)], dim=1)
             byte_length += get_byte_length(tokenizer, c_id)
 
         normalized += unnormalized / byte_length if byte_length > 0 else 0.0  # Byte-length normalized (https://blog.eleuther.ai/multiple-choice-normalization/)
