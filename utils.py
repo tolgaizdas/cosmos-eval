59f1cc7dd42c6062525e73bc84328fd3461a0887
Tolga İzdaş <tolgaizdas@gmail.com>
2024-08-15 13:31:35 +0300

    added perplexity function

--- utils.py
+++ utils.py
@@ -41,6 +41,42 @@ def get_results(model, tokenizer, prompt, choices, device):
         results_norm.append(normalized)
     return results, results_norm
 
+"""
+ https://huggingface.co/docs/transformers/en/perplexity
+ TODO: This function is taken from the Hugging Face documentation. It can be modified to be more efficient.
+"""
+def perplexity(model, tokenizer, text, device):
+    encodings = tokenizer(text, return_tensors="pt").to(device)
+
+    max_length = model.config.n_positions
+    stride = 512  # TODO: Stride can be an argument or a parameter of the model
+    seq_len = encodings.input_ids.size(1)
+
+    nlls = []
+    prev_end_loc = 0
+    for begin_loc in range(0, seq_len, stride):
+        end_loc = min(begin_loc + max_length, seq_len)
+        trg_len = end_loc - prev_end_loc  # May be different from stride on last loop
+        input_ids = encodings.input_ids[:, begin_loc:end_loc]
+        target_ids = input_ids.clone()
+        target_ids[:, :-trg_len] = -100  # Mask the tokens before the target sequence length
+
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+
+            # loss is calculated using CrossEntropyLoss which averages over valid labels
+            # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
+            # to the left by 1.
+            neg_log_likelihood = outputs.loss
+
+        nlls.append(neg_log_likelihood)
+
+        prev_end_loc = end_loc
+        if end_loc == seq_len:
+            break
+
+    ppl = torch.exp(torch.stack(nlls).mean())
+    return ppl.item()
 
 def load_model_and_tokenizer(model_name):
     model = AutoModelForCausalLM.from_pretrained(model_name)
